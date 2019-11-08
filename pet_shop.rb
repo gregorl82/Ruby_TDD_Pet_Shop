3b6cb9030b378badb657126ffa25f0fda1f5a3ab
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-08 14:47:15 +0000

    Adds pets_by_breed tests and function

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,13 @@ end
 def stock_count(shop)
   return shop[:pets].length
 end
+
+def pets_by_breed(shop, breed)
+  found_pets = []
+  for pet in shop[:pets]
+    if (pet[:breed] == breed)
+      found_pets.push(pet)
+    end
+  end
+  return found_pets
+end
